f7823e65189d5bd96f6399606cf5df2ebabd0107
TalebMaham <sidnas9@gmail.com>
2024-11-09 18:53:22 +0100

    requirements

--- app.py
+++ app.py
@@ -12,20 +12,20 @@ def load_projects():
 
 
 
-@app.route('/portfolio')
+@app.route('/')
 def home():
     projects_json = load_projects()
     return render_template('index.html', section="home", projects = projects_json )
 
-@app.route('/portfolio/section/about')
+@app.route('/section/about')
 def about():
     return jsonify({"section": "about"})
 
-@app.route('/portfolio/section/projects')
+@app.route('/section/projects')
 def projects():
     return jsonify({"section": "projects"})
 
-@app.route('/portfolio/section/contact')
+@app.route('/section/contact')
 def contact():
     return jsonify({"section": "contact"})
 
